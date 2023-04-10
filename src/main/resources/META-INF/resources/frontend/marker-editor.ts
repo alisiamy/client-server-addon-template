ed617f304b4286b0588778394bcadeef5603db8e
Daria Grisanova <darija.grisanova@st.ovgu.de>
2023-04-10 10:44:21 +0200

    add marker editing functionality on frontend component

--- src/main/resources/META-INF/resources/frontend/marker-editor.ts
+++ src/main/resources/META-INF/resources/frontend/marker-editor.ts
@@ -1,48 +1,96 @@
-import {css, html, LitElement, TemplateResult, PropertyValueMap } from 'lit';
+import {css, html, LitElement, TemplateResult, PropertyValueMap} from 'lit';
 import {property, customElement} from 'lit/decorators.js';
 import * as d3 from 'd3';
+import {readFileSync} from "fs";
 
 const drawAreaSelector = '#drawArea';
 
 @customElement('marker-editor')
 export class MarkerEditorElement extends LitElement {
-  private _svg : Object;
+    private _svg: SVGElement | undefined;
+    private line: SVGLineElement | undefined;
 
     render(): TemplateResult {
 
-        return html`
+        /*return html`
             <input type="button" value="go" @click=${this.doDraw} />
+            <div id="drawArea"></div>
+        `;*/
+        return html`
             <div id="drawArea"></div>
         `;
     }
 
-     constructor() {
+    constructor() {
         super();
+        //construct d3 gedöns
+
     }
 
-    doDraw() : void {
-        //plot kram
-           this._svg.append('line')
-                           .style("stroke", "lightgreen")
-                           .style("stroke-width", 10)
-                           .attr("x1", 0)
-                           .attr("y1", 0)
-                           .attr("x2", 200)
-                           .attr("y2", 200);
-   }
+    mousedown(ev: Event) {
+        if (ev.target != this)
+            return;
+        let m = d3.pointer(ev);
+        let sel = d3.select(this);
+        sel.append('circle')
+            .style("stroke", "lightgreen")
+            .style("fill", "transparent")
+            .attr('tabindex', '0')
+            .attr('focusable', 'true')
+            .attr("r", 10)
+            .attr("cx", m[0])
+            .attr("cy", m[1])
+            .on("mouseover", function (ev: Event) {
+                let l = d3.select(this);
+                l.style("stroke", "red");
+            })
+            .on("mouseout", function (ev: Event) {
+                let l = d3.select(this);
+                l.style("stroke", "lightgreen");
+            })
+            .on("mousedown", function (ev : Event) {
+                this.setAttribute("resizing", "");
+            })
+            .on("mousemove", function (ev :Event) {
+                let l = d3.select(this);
+                if (!this.hasAttribute("resizing"))
+                {
+                    return;
+                }
+                let m = d3.pointer(ev);
+                const cx = l.attr("cx");
+                const cy = l.attr("cy");
+                const ox = m[0];
+                const oy = m[1];
+                const r = 10+Math.ceil(Math.sqrt(((ox - cx) * (ox - cx)) + ((oy - cy) * (oy - cy))));
+                l.attr("r", r);
+            })
+            .on("mouseup", function (ev : Event) {
+                this.removeAttribute("resizing");
+
+            })
+            .on("keyup", function(ev:KeyboardEvent) {
+                if(ev.key == "Delete" || ev.key == "Del" || ev.key == "Backspace"){
+                    d3.select(this).remove();
+                }
+            });
+    }
+
+
 
     /** Invoked when a component is added to the document's DOM.
      *
      */
     connectedCallback(): void {
         super.connectedCallback();
-
-        //construct d3 gedöns
-        const element = this.shadowRoot.querySelector(drawAreaSelector);
-        this._svg = d3.select(element).append('svg').attr('width', 400)
-                                                                                           .attr('height', 400)
-                                                                                           .style('background-color', 'black');
-
+        const element = this.renderRoot.querySelector(drawAreaSelector);
+        this._svg = d3.select(element).append('svg');
+        this._svg.attr('width', 400);
+        this._svg.on("mousedown", this.mousedown);
+        //.on("mouseup", this.mouseup)
+        this._svg.attr('height', 400)
+        this._svg
+            .style("background", "url('https://www.google.nl/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png')");
     }
 
     /** Controls whether an update rendering should proceed.
